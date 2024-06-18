044a4ae30896258bc85eeee650209c2398d09eca
Farjana73 <faakther73@gmail.com>
2024-06-18 12:49:37 +0600

    added new

--- scrript.js
+++
@@ -1,8 +0,0 @@
-
-// Read data
-
-async function onRead(){
-    let data = await fetch('https://crud.teamrabbil.com/api/v1/ReadProduct');
-    let JsonData = await data.json();
-    console.log(JsonData);
-}
